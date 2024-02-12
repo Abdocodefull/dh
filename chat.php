ec5e191f57f97c607027f998d329afec498db0e1
Abdocodefull <154697576+Abdocodefull@users.noreply.github.com>
2024-02-12 22:49:09 +0200

    Add files via upload

---
+++ chat.php
@@ -0,0 +1,43 @@
+<?php 
+  session_start();
+  include_once "php/config.php";
+  if(Session::exists("register_success") && $user->getPropertyValue("username") == Session::get("new_username")) {
+    $welcomeMessage = Session::flash("new_username") . ", " . Session::flash("register_success");
+}
+?>
+<?php include_once "header.php"; ?>
+<body>
+  <div class="wrapper">
+    <section class="chat-area">
+      <header>
+        <?php 
+          $user_id = mysqli_real_escape_string($conn, $_GET['username']);
+          $sql = mysqli_query($conn, "SELECT * FROM user_info WHERE 	username = {$user_id}");
+          if(mysqli_num_rows($sql) > 0){
+            $row = mysqli_fetch_assoc($sql);
+          }else{
+            header("location: users.php");
+          }
+        ?>
+        <a href="users.php" class="back-icon"><i class="fas fa-arrow-left"></i></a>
+        <img src="php/images/<?php echo $row['img']; ?>" alt="">
+        <div class="details">
+          <span>@<?php echo $row['username'];?></span>
+          <p><?php echo $row['last_active_update']; ?></p>
+        </div>
+      </header>
+      <div class="chat-box">
+
+      </div>
+      <form action="#" class="typing-area">
+        <input type="text" class="incoming_id" name="incoming_id" value="<?php echo $user_id; ?>" hidden>
+        <input type="text" name="message" class="input-field" placeholder="Type a message here..." autocomplete="off">
+        <button><i class="fab fa-telegram-plane"></i></button>
+      </form>
+    </section>
+  </div>
+
+  <script src="javascript/chat.js"></script>
+
+</body>
+</html>
